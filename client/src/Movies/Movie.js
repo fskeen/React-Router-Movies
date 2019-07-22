70cae8f8e56565061ae7fafc32860ec80afb963d
Faye Skeen <fskeen@gmail.com>
2019-07-22 14:02:07 -0700

    Fixed infinite loop that would have caused problems. Opps!

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,7 +5,7 @@ const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   useEffect(() => {
-    let id = props.match.params.id;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -13,15 +13,14 @@ const Movie = (props) => {
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
-          console.log(response.data)
+          console.log(response.data);
         })
         .catch(error => {
           console.error(error);
         });
 
-  },[movie]);
+  },[props.match.params.id]);
 
-  console.log("test", movie)
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
